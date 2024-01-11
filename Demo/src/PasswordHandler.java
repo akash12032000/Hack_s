1e1ee8404b8bb71ec74b1abb7bf3e557ba5f8bfb
Akash Aggarwal <79623225+akash12032000@users.noreply.github.com>
2024-01-11 10:20:26 +0530

    Update PasswordHandler.java

--- Demo/src/PasswordHandler.java
+++ Demo/src/PasswordHandler.java
@@ -1,68 +1,55 @@
-package com.model;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESedeKeySpec;
-import java.io.UnsupportedEncodingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+public class PasswordHandler
+{
+    /* Private variable declaration */
+    private static final String SECRET_KEY = "ThisIsInsightgeekspvtKey";
+    private static final String SALTVALUE = "eulavtlas";
 
-public class PasswordHandler {
-    private static final String UNICODE_FORMAT = "UTF8";
-    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
-    private static KeySpec ks;
-    private static SecretKeyFactory skf;
-    private static Cipher cipher;
-    static byte[] arrayBytes;
-    private static String myEncryptionKey;
-    private static String myEncryptionScheme;
-    static SecretKey key;
-
-    private static void loadObjectValues() throws InvalidKeyException, InvalidKeySpecException, NoSuchPaddingException, NoSuchAlgorithmException, UnsupportedEncodingException {
-         /*
-        Key length should be 24 Character
-        */
-        myEncryptionKey = "ThisIsInsightgeekspvtKey";
-        myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
-        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
-        ks = new DESedeKeySpec(arrayBytes);
-        skf = SecretKeyFactory.getInstance(myEncryptionScheme);
-        cipher = Cipher.getInstance(myEncryptionScheme);
-        key = skf.generateSecret(ks);
+    public static String encrypt(String strToEncrypt) throws InvalidAlgorithmParameterException, NoSuchPaddingException, InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        return Base64.getEncoder().encodeToString(getCipher(Cipher.ENCRYPT_MODE).doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
     }
 
-    public static String encrypt(String unencryptedString) throws NoSuchPaddingException, UnsupportedEncodingException, InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException {
-        loadObjectValues();
-        String encryptedString = null;
-        try {
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
-            byte[] encryptedText = cipher.doFinal(plainText);
-            encryptedString = new String(Base64.getEncoder().encode(encryptedText));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return encryptedString;
+    public static String decrypt(String strToDecrypt) throws InvalidAlgorithmParameterException, NoSuchPaddingException, InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        return new String(getCipher(Cipher.DECRYPT_MODE).doFinal(Base64.getDecoder().decode(strToDecrypt)));
+    }
+    private static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        return new IvParameterSpec(iv);
     }
 
-    public static String decrypt(String encryptedString) throws NoSuchPaddingException, UnsupportedEncodingException, InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException {
-        loadObjectValues();
-        String decryptedText=null;
-        try {
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] encryptedText = Base64.getDecoder().decode(encryptedString);
-            byte[] plainText = cipher.doFinal(encryptedText);
-            decryptedText= new String(plainText);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return decryptedText;
+    private static Cipher getCipher(int MODE) throws InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(MODE, secretKey, generateIv());
+        return cipher;
     }
+    public static void main(String[] args) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, InvalidKeySpecException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
 
+        String originalval = "Encryption is the part of security";
+        String encryptedval = encrypt(originalval);
+        String decryptedval = decrypt(encryptedval);
+        System.out.println("Original value: " + originalval);
+        System.out.println("Encrypted value: " + encryptedval);
+        System.out.println("Decrypted value: " + decryptedval);
 
-}
+    }
+}  
